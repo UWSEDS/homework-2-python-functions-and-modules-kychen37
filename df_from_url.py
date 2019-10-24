bfeefad7e1d387a731b72f467dbef5a1c2b8de46
Katharine Chen <kychen37@uw.edu>
2019-10-24 00:28:40 -0700

    change code to iterate through rows in column to get type

--- df_from_url.py
+++ df_from_url.py
@@ -11,10 +11,22 @@ def test_create_dataframe(dataframe, col_list):
     count = 0
     if set(dataframe.columns) == set(col_list):
         count +=1
-    if all(isinstance(x,(object)) for x in dataframe.columns):
-        count +=1
+
     if len(dataframe) >= 10:
         count +=1
- 
+    
+    types = []
+    for col in dataframe:
+        types.append(type(dataframe.loc[0,col]))
+    counter = 0
+    failed_count = 0
+    for i,col in enumerate(dataframe):
+        for entry in dataframe[col]:
+            if type(entry) == types[i]:
+                counter +=1 
+            else: 
+                failed_count +=1
+    
+    if failed_count == 0: count += 1 
     if count == 3: print('True')
     else: print('False')
